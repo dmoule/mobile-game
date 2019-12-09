ac3e8ace9d0d636c061775b5ed57bd6298100ce7
Unknown <dmoule@hotmail.com>
2019-12-09 19:23:12 +0000

    Scripts
    adding death and respawn

---
+++ Mobile Game draft/Assets/PT_Prefabs/death.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class death : MonoBehaviour
+{
+    public GameObject GN;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name.Contains("Red")||col.gameObject.name.Contains("red"))
+        {
+            GN.GetComponent<GameManager>().dead = true;
+            Destroy(gameObject);
+            
+        }
+    }
+}
